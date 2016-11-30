1e3ed287e8a5b8c6217deb452eec8159835056a5
Brian Devins <badevins@gmail.com>
2016-11-30 06:38:18 -0500

    Method to list interfaces, not used yet

--- cmd/dash-detect/main.go
+++ cmd/dash-detect/main.go
@@ -1,25 +1,39 @@
 package main
 
-import(
-  "fmt"
+import (
+	"fmt"
+	"net"
 
-  "github.com/google/gopacket"
-  "github.com/google/gopacket/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 )
 
 func main() {
-  if handle, err := pcap.OpenLive("eno1", 1600, true, pcap.BlockForever); err != nil {
-    panic(err)
-  } else if err := handle.SetBPFFilter("arp"); err != nil {  // optional
-    panic(err)
-  } else {
-    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-    for packet := range packetSource.Packets() {
-      handlePacket(packet)  // Do something with a packet here.
-    }
-  }
+	if handle, err := pcap.OpenLive("en1", 1600, true, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter("arp"); err != nil {
+		// optional
+		panic(err)
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			handlePacket(packet) // Do something with a packet here.
+		}
+	}
+}
+
+func getDeviceNames() ([]string) {
+	result := make([]string, 5)
+
+	interfaces, _ := net.Interfaces()
+	for _, iface := range interfaces {
+		append(result, iface.Name)
+	}
+	
+	return result
 }
 
 func handlePacket(packet gopacket.Packet) {
-  fmt.Println("Packet received", packet.Dump())
+	// if packet.LinkLayer().
+	fmt.Println("Packet received", packet.Dump())
 }
